b77bc6e65b34eaf88aa43d7d88e22ac4b4f33a9a
Brian Howell <bhowell@berkeley.edu>
2021-07-16 13:06:14 -0600

    updated values for uncertainty formulation

--- drugProblem.py
+++ drugProblem.py
@@ -2,126 +2,131 @@ import cvxpy as cp
 import numpy as np
 
 
-def drugOpt(costs_):
-    cost1 = costs_[0]
-    cost2 = costs_[1]
-    a1 = [0.1, 0.0095, 0.01005]
-    a2 = [0.2, 0.0196, 0.0204]
-    A = np.array([[-a1[1], -a2[1], 0.5, 0.6],
-                  [1, 1, 0, 0],
-                  [0, 0, 90.0, 100.0],
-                  [0, 0, 40.0, 50.0],
-                  [100.0, 199.9, cost1, cost2]])
-    b = np.array([0, 1000, 2000, 800, 100000])
-    c = np.array([100, 199.9, cost1-5500, cost2-6100])
-
-    x = cp.Variable(4)
-
-    obj = cp.Minimize(c.T @ x)
-
-    constraints = [x >= 0,
-                   A@x <= b]
-
-    prob = cp.Problem(obj, constraints)
-    prob.solve()
-    optimalX = x.value
-    profit = prob.value
-
-    return optimalX, profit
-
-# opCost = np.array([[0, 0]])
-opCost = np.array([[0, 0],
-                   [100,150],
-                   [200, 275],
-                   [400, 500],
-                   [600, 675],
-                   [700, 800]])
-
-profitTot = []
-for i in range(len(opCost)):
-    optimalX_, profit_ = drugOpt(opCost[i, :])
-
-    profitTot.append(profit_)
-
-
-import matplotlib.pyplot as plt
-
-xplot = [np.sum(opCost[i, :]) for i in range(len(opCost[:, 0]))]
-
-print(xplot)
-profitTot = [-1*profitTot[i] for i in range(len(profitTot))]
-print('profitTot: ', profitTot)
-
-
-plt.figure()
-plt.plot(xplot, profitTot, '-o')
-plt.plot([-100, 2000], [0, 0], '-r')
-plt.ylabel('Profit ($)')
-plt.xlabel('Total Operating Costs ($)')
-
-# plt.show()
-plt.savefig('drugOpt.png')
-
-
-# """
-# # Uncertainty
-#
-# a1 = [0.0095, 0.01005]
-# a2 = [0.0196, 0.0204]
-# +a1[0]
-# +a2[0]
-# """
-# cost1 = 0
-# cost2 = 0
-# a1 = [0.0095, 0.01005]
-# a2 = [0.0196, 0.0204]
-# A = np.array([[-(a1[0]), -(a2[1]), 0.5, 0.6],
+# def drugOpt(costs_):
+#     cost1 = costs_[0]
+#     cost2 = costs_[1]
+#     a1 = [0.01, 0.0095, 0.01005]
+#     a2 = [0.02, 0.0196, 0.0204]
+#     A = np.array([[-a1[1], -a2[1], 0.5, 0.6],
 #                   [1, 1, 0, 0],
 #                   [0, 0, 90.0, 100.0],
 #                   [0, 0, 40.0, 50.0],
-#                   [100.0, 199.9, 700, 800]])
-# b = np.array([0, 1000, 2000, 800, 100000])
-# c = np.array([100, 199.9, cost1-5500, cost2-6100])
+#                   [100.0, 199.9, cost1, cost2]])
+#     b = np.array([0, 1000, 2000, 800, 100000])
+#     c = np.array([100, 199.9, cost1-5500, cost2-6100])
 #
-# x = cp.Variable(4)
+#     x = cp.Variable(4)
 #
-# obj = cp.Minimize(c.T @ x)
+#     obj = cp.Minimize(c.T @ x)
 #
-# constraints = [x >= 0,
-#                A@x <= b]
+#     constraints = [x >= 0,
+#                    A@x <= b]
 #
-# prob = cp.Problem(obj, constraints)
-# prob.solve()
-# robustX = x.value
-# robustProfit = -prob.value
+#     prob = cp.Problem(obj, constraints)
+#     prob.solve()
+#     optimalX = x.value
+#     profit = prob.value
 #
-# print('robustX: ', np.round(robustX, 5))
-# print('robustProfit: ', robustProfit)
-# print('')
+#     return optimalX, profit
+#
+# # opCost = np.array([[0, 0]])
+# opCost = np.array([[0, 0],
+#                    [100,150],
+#                    [200, 275],
+#                    [400, 500],
+#                    [600, 675],
+#                    [700, 800]])
+#
+# profitTot = []
+# for i in range(len(opCost)):
+#     optimalX_, profit_ = drugOpt(opCost[i, :])
+#
+#     profitTot.append(profit_)
+#
+#
+# import matplotlib.pyplot as plt
+#
+# xplot = [np.sum(opCost[i, :]) for i in range(len(opCost[:, 0]))]
+#
+# print(xplot)
+# profitTot = [-1*profitTot[i] for i in range(len(profitTot))]
+# print('profitTot: ', profitTot)
+#
+#
+# plt.figure()
+# plt.plot(xplot, profitTot, '-o')
+# plt.plot([-100, 2000], [0, 0], '-r')
+# plt.ylabel('Profit ($)')
+# plt.xlabel('Total Operating Costs ($)')
+#
+# # plt.show()
+# plt.savefig('drugOpt.png')
 
-# # original module2 solution
-#
-# cost1 = 700
-# cost2 = 800
-# A = np.array([[-0.01, -0.02, 0.5, 0.6],
-#               [1, 1, 0, 0],
-#               [0, 0, 90.0, 100.0],
-#               [0, 0, 40.0, 50.0],
-#               [100.0, 199.9, cost1, cost2]])
-# b = np.array([0, 1000, 2000, 800, 100000])
-# c = np.array([100, 199.9, cost1 - 5500, cost2 - 6100])
-#
-# x = cp.Variable(4)
-#
-# obj = cp.Minimize(c.T @ x)
-#
-# constraints = [x >= 0,
-#                A @ x <= b]
-#
-# prob = cp.Problem(obj, constraints)
-# prob.solve()
-# optimalX = x.value
-# profit = prob.value
-#
-# print('optimalX: ', (optimalX))
-# print('profit: ', profit)
+
+"""
+# Uncertainty
+
+a1 = [0.0095, 0.01005]
+a2 = [0.0196, 0.0204]
++a1[0]
++a2[0]
+"""
+cost1 = 0
+cost2 = 0
+a1 = [0.01, 0.00995, 0.01005]        # with box uncertainty
+a2 = [0.02, 0.0196, 0.0204]         # choose worst case scenario
+A = np.array([[-(a1[1]), -(a2[1]), 0.5, 0.6],
+                  [1, 1, 0, 0],
+                  [0, 0, 90.0, 100.0],
+                  [0, 0, 40.0, 50.0],
+                  [100.0, 199.9, 700, 800]])
+b = np.array([0, 1000, 2000, 800, 100000])
+c = np.array([100, 199.9, cost1-5500, cost2-6100])
+
+x = cp.Variable(4)
+
+obj = cp.Minimize(c.T @ x)
+
+constraints = [x >= 0,
+               A@x <= b]
+
+prob = cp.Problem(obj, constraints)
+prob.solve()
+robustX = x.value
+robustProfit = -prob.value
+print('robustX: ', np.round(robustX, 3))
+print('robustProfit: ', robustProfit)
+print('')
+
+
+"""
+# original module2 solution
+---* to obtain p* list in problem statement, set A[4, 3] = -5500, and A[4, 4] = -6100
+---* which is in correct since it is not taking into account the operating costs of drug1 and drug2
+---*
+"""
+cost1 = 700
+cost2 = 800
+A = np.array([[-0.01, -0.02, 0.5, 0.6],
+              [1, 1, 0, 0],
+              [0, 0, 90.0, 100.0],
+              [0, 0, 40.0, 50.0],
+              [100.0, 199.9, cost1, cost2]])
+b = np.array([0, 1000, 2000, 800, 100000])
+c = np.array([100, 199.9,  - 5500,  - 6100])
+
+x = cp.Variable(4)
+
+obj = cp.Minimize(c.T @ x)
+
+constraints = [x >= 0,
+               A @ x <= b]
+
+prob = cp.Problem(obj, constraints)
+prob.solve()
+optimalX = x.value
+profit = -prob.value
+
+print('original module 2 formulation with error:')
+print('optimalX: ', (optimalX))
+print('profit: ', profit)
